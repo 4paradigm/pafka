d9315d6a3b3df9716370f58a3bba1838d6b91ae3
Zhang Hao <zhanghao@4paradigm.com>
2021-06-08 10:25:37 +0800

    change docker tag

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -21,17 +21,10 @@ if [[ $# -lt 1 ]]; then
 fi
 
 tag=$1
-repo=pafka/pafka-dev
+repo=4pdopensource/pafka-dev
 cache_image="$repo:latest"
 docker pull ${cache_image} || true
 docker build --cache-from ${cache_image} -t $repo:$tag -f docker/Dockerfile .
 docker tag $repo:$tag $repo:latest
 docker push $repo:$tag
 docker push $repo:latest
-
-# also push to repo2
-repo2=4pdopensource/pafka
-docker tag $repo:$tag $repo2:$tag
-docker tag $repo2:$tag $repo2:latest
-docker push $repo2:$tag
-docker push $repo2:latest
